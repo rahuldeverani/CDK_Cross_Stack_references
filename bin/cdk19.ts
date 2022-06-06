1c7929bd3638be7660812410fcfa8c19afceec83
rahuldeverani <rahuldeverani@gmail.com>
2022-06-06 11:28:23 +0530

    add sg vpc

--- bin/cdk19.ts
+++ bin/cdk19.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { SecurityGroupstack } from '../lib/SecurityGroupstack';
 import { eksStack } from '../lib/eksStack';
 
-const envUSA  = { account: '17xxxxxxx', region: 'us-east-1' };
+const envUSA  = { account: '17xxxxx', region: 'us-east-1' };
 const app = new cdk.App();
 const sgstack =new SecurityGroupstack(app, 'sgStack1',{ env: envUSA });
 new eksStack(app, 'eksStack1' , {env: envUSA, sg:sgstack.secg})
